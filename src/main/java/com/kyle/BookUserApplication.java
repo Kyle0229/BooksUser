f5b59b12b378103ce318b1cd3ffafd849fd3d02e
kyle <824238868@qq.com>
2019-11-11 09:43:24 +0800

    Initial commit

---
+++ src/main/java/com/kyle/BookUserApplication.java
@@ -0,0 +1,11 @@
+package com.kyle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookUserApplication {
+    public static void main(String[] args){
+        SpringApplication.run(BookUserApplication.class);
+    }
+}
